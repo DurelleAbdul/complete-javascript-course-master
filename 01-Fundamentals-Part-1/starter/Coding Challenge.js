79ee5f96e7f3917d5734f065dbf98ce4ef70a91c
Durelle <Durelle.abdul.07@gmail.com>
2021-09-01 15:43:55 +0200

    Adding the code

--- 01-Fundamentals-Part-1/starter/Coding Challenge.js
+++ 01-Fundamentals-Part-1/starter/Coding Challenge.js
@@ -1,19 +1,72 @@
-let markHeight = 1.88; // height in m
-let markMass = 95;  //mass in kg
+/*Coding Challenge #1
 
-let johnHeight = 1.76; // height in m
-let johnMass = 85;  //mass in kg
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
 
-let markBMI = markMass / (markHeight * 2);
-let johnBMI = johnMass / (johnHeight * 2);
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
 
-let markHigherBMI = false;
 
-if (markBMI > johnBMI) markHigherBMI = true;
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK �
 
-console.log("Mark BMI: " + markBMI);
-console.log("John BMI: " + johnBMI);
 
-if (markHigherBMI === true) console.log("Marks BMI is Higher than Johns")
-else console.log("Johns BMI is higher then Marks");
+let markWeight = 95;
+let markHeight = 1.88;
+
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+let durelleWeight = 130;
+let durelleHeight = 1.76;
+
+markBMI = markWeight / (markHeight ** 2);
+johnBMI = johnWeight / (johnHeight ** 2);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
+
+
+//Coding challenge #2
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than Johns(${johnBMI})`);
+}
+else {
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's BMI(${markBMI})`);
+}
+
+
+const dolphinAvg = (97 + 112 + 101) / 3;
+const KoalasAvg = (109 + 95 + 106) / 3;
+
+if (dolphinAvg > KoalasAvg && dolphinAvg >= 100) {
+    console.log(`Dolphin's win with an average of ${dolphinAvg}`);
+}
+else if (KoalasAvg > dolphinAvg && KoalasAvg >= 100) {
+    console.log(`Koala's win with an average of ${KoalasAvg}`);
+}
+else if (dolphinAvg === KoalasAvg && dolphinAvg >= 100 && KoalasAvg >= 100) {
+    console.log(`Its a DRAW!!!!`);
+}
+else {
+    console.log(`No one wins :(`);
+}
+
+console.log(`${dolphinAvg} and ${KoalasAvg}`);
+*/
+
+
+
+
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -38,15 +38,257 @@ year = 1996;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
-let age = 30;
-age = 31;
+
+let age = 25;
+age = 26;
 
 const birthyear = 1996;
-// birthyear = 1997;
+//   birthyear = 1997; const values cant change
+// const job; You need an initial value!!!
+// Good practice to use const by default
 
-const job;
+// Var is the old way to declare variables
+var job = "programmer";
+job = "LOl";
 
-var job = 'programmer';
-job = "Manager";
+// this creates a property on the global
+lastName = "Abdul";
+console.log(lastName);
+
+
+//Basic Operators
+const now = 2021;
+
+const ageDurelle = now - 1996;
+const ageCayla = now - 2001;
+console.log(ageDurelle, ageCayla);
+
+console.log(ageDurelle * 2, ageCayla / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Durelle";
+const lastName = "Abdul";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100;
+x++ // x = x + 1 = 26
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+
+console.log(ageDurelle > ageCayla); // <, >, >=, <=
+console.log(ageCayla >= 21); //includes it in the range
+
+const isFullAge = ageCayla >= 21;
+console.log((now - 1991) > (now - 2019));
+
+
+// Operator Precendence  MDN web docs developer.mozilla.org
+
+const now = 2021;
+const ageDurelle = now - 1996;
+const ageCayla = now - 2001;
+console.log(now - 1996 < now - 2001);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = Y = 10;
+console.log(x, y);
+
+const averageAge = (ageDurelle + ageCayla) / 2;
+console.log(ageDurelle, ageCayla, averageAge);
+
+
+// 17: Strings and temperal literals
+
+const firstName = "Durelle";
+const job = "Programmer";
+const birthyear = 1996;
+const year = 2021;
+
+const durelle = "I'm " + firstName + ", a " + (year - birthyear) + " year old " + job;
+
+console.log(durelle);
+
+// temperal literal
+const durelleNew = `I'm ${firstName}, a ${year - birthyear} year old ${job}`;
+console.log(durelleNew);
+
+console.log(`Just a regular string with back-ticks`);
+
+console.log('String with \n\
+multiple\n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
+
+
+const age = 15;
+
+if (age >= 18) {
+    console.log("Durelle can start getting his Drivers License");
+}
+else {
+    console.log(`Durelle can only get his drivers license in ${18 - age} years`);
+}
+
+const birthyear = 1996;
+let century;
+if (birthyear <= 2000) {
+    century = 20;
+}
+else if (birthyear > 2000 && birthyear < 3000) {
+    century = 21;
+}
+console.log(`born in the ${century}th/st century`);
+
+
+// Type Conversion and type Coercion
+// Coercion is when Javascript automatically converts data types automatically for us
+
+const inputYear = '1996';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+console.log(Number('Durelle')); //NaN (invalid number)
+console.log(String(23), 23);
+
+//Type Coercion
+console.log('I am ' + 23 + ' ' + 'years old');//Coercion
+console.log('23' - '10' + 3);
+console.log('23' * '2');
+console.log('23' > '18');
+
+let n = '1' + 1; //'11'
+n = n - 1;
+console.log(n);
+
+
+// Truthy and falsy values
+// 5 falsy values = 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Durelle'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+
+if (money) {
+    console.log("Don't spend it all ;)");
+}
+else
+    console.log('You should get a job!');
+
+let height;
+if (height) {
+    console.log('Yay! type is defines');
+}
+else
+    console.log('Height is UNDEFINED');
+
+
+
+//  Equality operators === and ==
+const age = '18';
+
+if (age === 18)
+    console.log('Person is exactly 18 (strict)');
+
+if (age == 18)
+    console.log('Person is exactly 18 (loose)');
+// == uses type coercion
+
+const favNumber = Number(prompt("what's your favourite number?"));
+console.log(favNumber);
+
+if (favNumber === 23) // '23' == 23
+{
+    console.log('Cool! 23 is an amazing number!');
+}
+else if (favNumber === 7) {
+    console.log('Nice! just like mine');
+}
+else console.log("seems ok!!");
+
+if (favNumber !== 7) console.log("why not 7?");
+
+
+
+// Logical operations
+
+const hasDrivers = true;
+const hasGoodVision = true;
+
+console.log(hasDrivers && hasGoodVision);
+console.log(hasDrivers || hasGoodVision);
+console.log(!hasDrivers);
+
+const shouldDrive = hasDrivers && hasGoodVision;
+
+// if (shouldDrive) {
+//     console.log('Sarah can drive');
+// }
+// else console.log('Someone else should drive');
+
+const isTired = false;
+console.log(hasDrivers && hasGoodVision && isTired);
+
+
+if (hasDrivers && hasGoodVision && !isTired) {
+    console.log('Sarah can drive');
+}
+else {
+    console.log('Someone else should drive');
+}
+*/
+
+//Switch Statemt
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log(`Plan the course structure`);
+        console.log(`Complete an assignment`);
+        break;
+    case 'tuesday':
+        console.log(`Prepare food planning`);
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log(`Study!!`);
+        break;
+    case 'friday':
+        console.log(`Yaaaaaay it's friday!!`)
+        break;
+    case 'saturday':
+    case 'Sunday':
+        console.log(`Enjoy the weekend!`);
+        break;
+    default:
+        console.log(`Not a valid day!`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
